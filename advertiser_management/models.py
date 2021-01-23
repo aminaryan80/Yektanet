1b903b5223e92897eacdebf4b2b0c0f0e6e9c0d6
aminaryan80 <aryanmohammadamin@gmail.com>
2021-01-23 13:19:04 +0330

    add models

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertiser(models.Model):
+    name = models.CharField(max_length=200)
+    clicks = models.IntegerField(default=0, null=False)
+    views = models.IntegerField(default=0, null=False)
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=200)
+    img_url = models.ImageField(upload_to="ads/")
+    link = models.URLField(max_length=200)
+    clicks = models.IntegerField(default=0, null=False)
+    views = models.IntegerField(default=0, null=False)
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
